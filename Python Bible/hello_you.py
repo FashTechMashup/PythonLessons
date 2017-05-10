1bc74262b8e118c9c2469e9eb336f503602c20e0
Crystal Crawford <kmist@Crystals-MacBook-Pro.local>
2017-05-10 16:32:17 -0500

    new completed lessons

--- Python Bible/hello_you.py
+++ Python Bible/hello_you.py
@@ -11,5 +11,8 @@ city = input("What city do you live in?: ")
 love = input("What do you love doing?: ")
 
 # Create output text
+string = "Your name is {} and you are {} years old. You live in {} and you love {}."
+output = string.format(name,age,city,love)
 
 # Print output to screen
+print (output)
---
+++ Python Bible/how_old_are_you.py
@@ -0,0 +1,19 @@
+start = "I am "# First, create a variable called start, and set it equal to "I am ".
+# Remember the space after the word "am" !
+
+
+age = 24# Next, create a variable called age and set it equal to your age in years.
+# This must be a number
+
+
+end = " years old"# Next, create a variable called end, and set it equal to " years old".
+# Remember the space before the word "years"
+
+
+output = "{}{}{}".format (start, age, end)# Next, create a variable called output, and use the format() function to add
+# together the start, age and end variables. 
+
+# An example output string would be "I am 15 years old"
+
+
+print (output)# Finally, print the output to the screen using the print() function.
